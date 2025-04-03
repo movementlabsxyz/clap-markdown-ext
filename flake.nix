27c3af9a41be81ac4021bd34e44f4d28d0f7c768
Liam Monninger <l.mak.monninger@gmail.com>
2025-04-03 10:45:38 +0800

    fix: nix flake and dir.

--- flake.nix
+++ flake.nix
@@ -110,7 +110,7 @@
               export PATH="$PATH:$(pwd)/target/release"
 
               # Copy over ./githooks/pre-commit to .git/hooks/pre-commit
-              cp ./githooks/pre-commit .git/hooks/pre-commit
+              cp $(pwd)/.githooks/pre-commit $(pwd)/.git/hooks/pre-commit
 
               cat <<'EOF'
                 # CMDext
